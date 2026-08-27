39f93cacd066772a1c2fc7ea6968d2e65b5f174c
sonder <112090565+Alpenl@users.noreply.github.com>
2026-08-27 19:05:21 +0800

    Set current release version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.openaria.openaria_echo_mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "0.1.1"
     }
 
     compileOptions {
